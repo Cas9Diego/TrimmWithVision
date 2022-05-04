044874c2c1788b87bb786b19ab1f2aa6be7b4b7c
Diego Castro <99818957+Cas9Diego@users.noreply.github.com>
2022-05-04 09:53:16 +0200

    Add files via upload

---
+++ TrimmWithVision/TrimmWithVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimmWithVisionApp.swift
+//  TrimmWithVision
+//
+//  Created by Diego Castro on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TrimmWithVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
